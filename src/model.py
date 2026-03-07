8e7af7c3f1b3765c2df7ded463bcd744277f5223
FrancescoOrlando04 <francesco.orlando3@stud.unifi.it>
2026-03-07 19:32:45 +0100

    correzione Modello  CNN e modfica grafici

--- src/model.py
+++ src/model.py
@@ -5,23 +5,46 @@ class GalaxyCNN(nn.Module):
     def __init__(self, in_channels=3, num_classes=3):
         super(GalaxyCNN, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
-        self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, 16, 3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
 
-        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
 
-        # Con IMG_SIZE = 256 e 4 pool: 256 -> 128 -> 64 -> 32 -> 16
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.dropout = nn.Dropout(0.4)
+
         self.fc1 = nn.Linear(128 * 16 * 16, 256)
         self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
 
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
+        x = self.dropout(F.relu(self.fc1(x)))
         return self.fc2(x)
+
